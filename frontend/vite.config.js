c85014e36bb33059901de85e4559d2b963d2c70a
Vital <vital.15@mail.ru>
2026-06-06 19:45:10 +0300

    frontend feat: preliminary configuration of styles and installation of libraries

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,16 +11,19 @@ export default defineConfig({
 				// Автоматически внедряет указанные файлы во все ваши .scss и .module.scss файлы.
 				// Больше не нужно вручную писать @use во всех компонентах!
 				additionalData: `
-          @use "@/assets/styles/_constants.scss" as *;
-          @use "@/assets/styles/_functions.scss" as *;
-        `,
+	        @use "@/assets/styles/_variables.scss" as *;
+	        @use "@/assets/styles/_functions.scss" as *;
+	        @use "@/assets/styles/_mixins.scss" as *;
+	      `,
 			},
 		},
 	},
 	resolve: {
 		alias: {
 			// Настройка удобного алиаса '@', чтобы не писать выходы из папок '../../../../'
-			'@': '/src',
+			// fileURLToPath превращает URL в абсолютный путь файловой системы
+			// '@': '/src',
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
 })
