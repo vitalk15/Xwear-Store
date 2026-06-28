6091375286d4425b23c17715b7177bcd65e5a70f
Vital <vital.15@mail.ru>
2026-06-28 15:03:41 +0300

    frontend refactor: NavigationSkeleton

--- frontend/src/components/common/Header/NavigationSkeleton/NavigationSkeleton.jsx
+++ frontend/src/components/common/Header/NavigationSkeleton/NavigationSkeleton.jsx
@@ -1,4 +1,4 @@
-import styles from './Navigation.module.scss'
+import styles from './NavigationSkeleton.module.scss'
 
 const NavigationSkeleton = () => {
 	// Рисуем 4 фейковых пунктов меню, пока настоящие грузятся
