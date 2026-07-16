b7976827e195d8f42983d45d35b15fee20206bff
Vital <vital.15@mail.ru>
2026-07-16 19:43:58 +0300

    feat: pagination size increased to 12

--- django-app/xwear_shop/settings.py
+++ django-app/xwear_shop/settings.py
@@ -382,7 +382,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     # Пагинация
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 12,
 }
 
 
