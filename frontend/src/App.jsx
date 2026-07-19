e35b3f17a2347d1a762fdddfa09075bcb1dbccd1
Vital <vital.15@mail.ru>
2026-07-19 13:46:59 +0300

    frontend feat: added page scrolling up when navigating

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './components/common/ScrollToTop'
 import AppRoutes from './routes/AppRoutes'
 
 const App = () => {
 	return (
 		<BrowserRouter>
+			<ScrollToTop />
 			<AppRoutes />
 		</BrowserRouter>
 	)
