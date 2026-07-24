9c4c54c1331ef15853456ca11e4ba33c810ad119
Vital <vital.15@mail.ru>
2026-07-24 21:55:32 +0300

    frontend feat: replaced the arrow in FilterSection

--- frontend/src/features/catalog/components/CatalogSidebar/FilterSection.jsx
+++ frontend/src/features/catalog/components/CatalogSidebar/FilterSection.jsx
@@ -21,8 +21,22 @@ const FilterSection = ({ title, children, defaultOpen = true }) => {
 				type="button"
 			>
 				<span className={styles.sectionTitle}>{titleFilter}</span>
-				{/* Простая стрелочка, которая будет крутиться через CSS */}
-				<span className={`${styles.arrow} ${isOpen ? styles.open : ''}`}>►</span>
+				{/* стрелочка, которая будет крутиться через CSS */}
+				<svg
+					className={`${styles.arrow} ${isOpen ? styles.open : ''}`}
+					width="12"
+					height="8"
+					viewBox="0 0 12 8"
+					fill="none"
+				>
+					<path
+						d="M1 1.5L6 6.5L11 1.5"
+						stroke="currentColor"
+						strokeWidth="1.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+				</svg>
 			</button>
 
 			{isOpen && <div className={styles.sectionContent}>{children}</div>}
