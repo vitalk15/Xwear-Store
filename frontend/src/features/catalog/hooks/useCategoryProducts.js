07d19aee56ef973658b5583582dc2cd0fb09643f
Vital <vital.15@mail.ru>
2026-08-24 14:05:57 +0300

    frontend fix: fixed useCategoryProducts

--- frontend/src/features/catalog/hooks/useCategoryProducts.js
+++ frontend/src/features/catalog/hooks/useCategoryProducts.js
@@ -1,15 +1,12 @@
-import { useSuspenseQuery, skipToken } from '@tanstack/react-query'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { fetchCategoryProducts } from '../api/productApi'
 
 export const useCategoryProducts = (categoryId, params = {}) => {
-	// Разрешаем запрос, если передана категория или если идет текстовый поиск
-	const shouldFetch = categoryId || params.search
-
 	return useSuspenseQuery({
 		queryKey: ['categoryProducts', categoryId, params],
-		// Если ничего нет, передаем skipToken,
-		// чтобы хук "подождал" и не делал ошибочный запрос к бэкенду.
-		queryFn: shouldFetch ? () => fetchCategoryProducts(categoryId, params) : skipToken,
+
+		// Если categoryId === null, API-функция сама сделает запрос на глобальный эндпоинт /shop/products/
+		queryFn: () => fetchCategoryProducts(categoryId, params),
 		// Кэшируем на 5 минут
 		staleTime: 5 * 60 * 1000,
 	})
