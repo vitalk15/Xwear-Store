35ac8008039eb13e7f33f3483890c4411eb064ba
Vital <vital.15@mail.ru>
2026-09-05 00:30:31 +0300

    frontend feat: updated useAuthStore

--- frontend/src/features/auth/store/useAuthStore.js
+++ frontend/src/features/auth/store/useAuthStore.js
@@ -1,28 +1,28 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-// Стор для управления авторизацией (хранения access-токена, данных вошедшего пользователя и статуса авторизации)
-const useAuthStore = create((set) => ({
-	user: null,
-	accessToken: null,
-	isAuthenticated: false,
-
-	// Установка данных после успешного входа или активации
-	setAuth: ({ user, access }) => {
-		set({
-			user,
-			accessToken: access,
-			isAuthenticated: true,
-		})
-	},
-
-	// Выход из системы
-	logout: () => {
-		set({
+// Стор для управления авторизацией (хранения access-токена, данных вошедшего пользователя и статуса авторизации). Persist - для автоматического сохранения данных в localStorage (чтобы они пережили перезагрузку сайта).
+const useAuthStore = create(
+	persist(
+		(set) => ({
 			user: null,
-			accessToken: null,
+			access: null,
 			isAuthenticated: false,
-		})
-	},
-}))
+
+			// Установка данных после успешного входа или активации
+			setAuth: ({ user, access }) => {
+				set({ user, access, isAuthenticated: true })
+			},
+
+			// Выход из системы
+			logout: () => {
+				set({ user: null, access: null, isAuthenticated: false })
+			},
+		}),
+		{
+			name: 'auth-storage', // Ключ, по которому данные будут лежать в localStorage
+		},
+	),
+)
 
 export default useAuthStore
