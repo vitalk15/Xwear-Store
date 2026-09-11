bf886f7713874c49cff987f0d5a1453088d6c7c7
Vital <vital.15@mail.ru>
2026-09-11 23:04:19 +0300

    frontend feat: QueryClient has been moved out of main.jsx.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './shared/api/queryClient'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App.jsx'
 import '@/assets/styles/global.scss'
-
-// 1. Создаем глобальный экземпляр клиента
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			// Отключаем автоматический повторный запрос при возвращении на вкладку браузера
-			// (чтобы сервер не дергался каждый раз, когда вы переключаетесь между окнами)
-			refetchOnWindowFocus: false,
-
-			// Если сервер ответил ошибкой, делаем только 1 повторную попытку (по умолчанию их 3)
-			retry: 1,
-		},
-	},
-})
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
