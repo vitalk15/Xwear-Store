e6252c32c96526c31152a56d1f5befff112d1d30
Vital <vital.15@mail.ru>
2026-09-12 15:41:08 +0300

    frontend feat: added FavoriteSkeleton

--- frontend/src/pages/FavoritesPage/FavoritesSkeleton.jsx
+++ frontend/src/pages/FavoritesPage/FavoritesSkeleton.jsx
@@ -1,5 +1,28 @@
-// import styles from './FavoritesPage.module.scss'
+import ProductCardSkeleton from '@/features/catalog/components/ProductCard/ProductCardSkeleton'
+import styles from './FavoritesPage.module.scss'
 
-const FavoritesSkeleton = () => {}
+const FavoritesSkeleton = () => {
+	// Имитируем пагинацию: 4 пустых карточек
+	const skeletonCards = Array.from({ length: 4 }, (_, i) => i)
+
+	return (
+		<>
+			{/* Имитация хлебных крошек */}
+			<div className={styles.breadcrumbsSkeleton} />
+
+			{/* Имитация заголовка и количества товаров */}
+			<div className={styles.header}>
+				<div className={styles.titleSkeleton} />
+				<div className={styles.countSkeleton} />
+			</div>
+			{/* Имитация карточек */}
+			<div className={styles.grid}>
+				{skeletonCards.map((index) => (
+					<ProductCardSkeleton key={index} />
+				))}
+			</div>
+		</>
+	)
+}
 
 export default FavoritesSkeleton
